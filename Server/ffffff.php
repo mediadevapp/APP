216a66c41eef92f10f1e9de1278b0c3c79ed7156
Peter-S-China <runlevel@163.com>
2015-01-12 16:14:54 +0800

    sort

--- Server/ffffff.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$a=array("a"=>"Dog","b"=>"Cat");
-array_push($a,"Horse","Bird");
-print_r($a);
-
-
-?>
--- Server/test副本.php
+++
@@ -1,6 +0,0 @@
-<form action="book1.php" method="post"> 
-
-
-<input type="text" name="name" value="raymond" id="name"/>
-<input type="submit" name="btn_submit" value="提交"/>
-</form> 
